bffde8fc59979f1d76c326f333921711a7819887
Tova Revach <tova5343@gmail.com>
2026-07-19 01:18:40 +0300

    add addEvent function

--- database/theGame.js
+++ database/theGame.js
@@ -2,7 +2,10 @@ eventsBottun()
 
 function eventsBottun() {
     document.getElementById('addEvent').addEventListener('click', addEnent)
-    document.getElementById('deleteEvent').addEventListener('click', deleteEvent)
     document.getElementById('updateEvent').addEventListener('click', updateEvent)
+    document.getElementById('deleteEvent').addEventListener('click', deleteEvent)
 }
 
+function addEvent() {
+    console.log("add event funcion");
+}
