e72b384dd667f64ee03c5e323e789569ce6ceac8
Tova Revach <tova5343@gmail.com>
2026-07-19 01:25:24 +0300

    add all the function off events
    delete
    update
    add

--- database/theGame.js
+++ database/theGame.js
@@ -1,11 +1,19 @@
 eventsBottun()
 
 function eventsBottun() {
-    document.getElementById('addEvent').addEventListener('click', addEnent)
+    document.getElementById('addEvent').addEventListener('click', addEvent)
     document.getElementById('updateEvent').addEventListener('click', updateEvent)
     document.getElementById('deleteEvent').addEventListener('click', deleteEvent)
 }
 
 function addEvent() {
     console.log("add event funcion");
+}
+
+function deleteEvent() {
+    console.log("delete event funcion");
+}
+
+function updateEvent() {
+    console.log("update event funcion");
 }
