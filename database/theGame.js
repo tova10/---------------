679dc2f6fe6d23e6ffcbd876c90088513ebe1b1b
Tova Revach <tova5343@gmail.com>
2026-07-19 01:17:00 +0300

    add the event listner
    ------[]------

---
+++ database/theGame.js
@@ -0,0 +1,8 @@
+eventsBottun()
+
+function eventsBottun() {
+    document.getElementById('addEvent').addEventListener('click', addEnent)
+    document.getElementById('deleteEvent').addEventListener('click', deleteEvent)
+    document.getElementById('updateEvent').addEventListener('click', updateEvent)
+}
+
